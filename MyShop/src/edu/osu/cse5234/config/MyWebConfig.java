be7082625df9b3f3a815599d2ecab7972c2bba38
silvana <garciaserrano.1@osu.edu>
2019-09-28 14:39:41 -0400

    did a bunch of work (useful message i know)

--- MyShop/src/edu/osu/cse5234/config/MyWebConfig.java
+++ MyShop/src/edu/osu/cse5234/config/MyWebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -24,7 +25,8 @@ public class MyWebConfig extends WebMvcConfigurerAdapter {
 	
 	@Bean
 	public InternalResourceViewResolver getInternalResourceViewResolver() {        
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();        
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();   
+		//resolver.setViewClass(JstlView.class);
 		resolver.setPrefix("/WEB-INF/jsp/");        
 		resolver.setSuffix(".jsp");
 		return resolver;    
