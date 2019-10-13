575d010f613f854b0339eda3ac4c1594bd7b8505
silvana <garciaserrano.1@osu.edu>
2019-10-13 00:28:32 -0400

    Started objective 1 and 2 lab 7

---
+++ MyShop/src/edu/osu/cse5234/business/OrderProcessingServiceBean.java
@@ -0,0 +1,29 @@
+package edu.osu.cse5234.business;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import edu.osu.cse5234.model.Order;
+
+/**
+ * Session Bean implementation class OrderProcessingServiceBean
+ */
+@Stateless
+@LocalBean
+public class OrderProcessingServiceBean {
+
+    /**
+     * Default constructor. 
+     */
+    public OrderProcessingServiceBean() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+     * Default constructor. 
+     */
+    public String processOrder(Order order) {
+		return null;
+        // TODO Auto-generated constructor stub
+    }
+}
